438f15e3613b1131cd0853879c36cb4dfe14b1e8
yzchen14 <yzchen14@gmail.com>
2023-10-03 23:50:58 +0800

    OKOK

---
+++ vue_test/components/test-component.js
@@ -0,0 +1,19 @@
+export const TestComponent = {
+
+
+    template:`
+    <div>
+        <span>{{text}}</span>
+    </div>
+    `
+    ,
+
+    data(){
+        return {
+            text: "OK"
+        }
+
+    },
+
+
+} 
